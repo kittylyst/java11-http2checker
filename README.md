2cfe07747ea19892341cb47e17e6d1f057681f57
Ben Evans <benjamin.john.evans@gmail.com>
2018-09-13 22:35:36 -0400

    Initial import

---
+++ README.md
@@ -0,0 +1,3 @@
+## HTTP2Checker
+
+A simple modular Java 11 tool for checking if sites are running HTTP/2
---
+++ src/http2checker/javanut7/ch12/HTTP2Checker.java
@@ -0,0 +1,26 @@
+package javanut7.ch12;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.Charset;
+
+//tag::CHECKER[]
+public final class HTTP2Checker {
+    public static void main(String[] args) throws Exception {
+        if (args.length == 0) {
+            System.err.println("Provide URLS to check");
+        }
+        for (final var location : args) {
+            var client = HttpClient.newBuilder().build();
+            var uri = new URI(location);
+            var req = HttpRequest.newBuilder(uri).build();
+
+            var response = client.send(req,
+                    HttpResponse.BodyHandlers.ofString(Charset.defaultCharset()));
+            System.out.println(location +": "+ response.version());
+        }
+    }
+}
+//end::CHECKER[]
